f297accc91d2be36b0b29e8bd5eb640b5c0e892b
Hakim ullah web developer <hakimullahdeveloper123@gmail.com>
2025-06-25 00:38:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,238 @@
+// TASK 1 //
+// GENERATE AND DISPLAY 10 RANDOM INTEGERS BETWEEN 1 AND 100 USING A FOR LOOP //
+// for (let int = 0; int < 10; int++) {
+//     let random = Math.floor(Math.random() * 100) + 1;
+//     console.log(random);
+// }
+// TASK 1 //
+
+
+
+
+
+
+
+
+// TASK 2 //
+// SIMULATE ROLLING A 6-SIDED DICE 20 TIMES AND PRINT THE RESULT OF EACH ROLL //
+// for (let int = 0; int < 20; int++) {
+//     let random = Math.floor(Math.random() * 6) + 1;
+//     console.log(random);
+// }
+// TASK 2 //
+
+
+
+
+
+
+
+// TASK 3 //
+// CREATE A PROGRAM THAT GENERATES 10 RANDOM EVEN NUMBERS BETWEE 2 AND 50 //
+// for (let i = 0; i < 10; i++) {
+//         let random = Math.floor(Math.random() * 25) + 1
+//         let evenNumber = random * 2;
+//         console.log(`Random Even Numbers ${evenNumber}`);
+//     }
+// TASK 2 //
+
+
+
+
+
+
+//   MY EXTRA WORK GENERATING ODD NUMBERS //   
+//   for (let i = 0; i < 10; i++) {
+//     let random = Math.floor(Math.random() * 25) + 1
+//     let oddNumber = random * 2 - 1;
+//     console.log(`Random Odd Numbers ${oddNumber}`);
+// }
+//   MY EXTRA WORK GENERATING ODD NUMBERS //   
+
+
+
+
+
+
+
+
+
+// MY EXTRA WORK GENERATING BOTH EVEN AND ODD NUMBERS // 
+// for (let i = 0; i < 10; i++) {
+//     let random = Math.floor(Math.random() * 25) + 1
+//     if (Math.random() < 0.5) {
+//         number = random * 2;
+//         console.log(`Random Even Numbers: ${number}`);
+//     } else {
+//         number = (random * 2) - 1;
+//         console.log(`Random odd numbers: ${number}`);
+//     }
+// }
+
+
+
+
+
+
+
+// TASK 4 //  
+// WRITE A PROGRAM THAT GENERATES 5 RANDOM NUMBERS BETWEEN 1 AND 20 , ADD THEM TOGETHER AND DISPLAY THE TOTAL SUM //
+// let sum = 0;
+// for (let i = 0; i < 5; i++) {
+//     let randomNum = Math.floor(Math.random() * 20) + 1;
+//     let result = sum + randomNum;
+//     console.log(result);
+// }
+// TASK 4 //  
+
+
+
+
+
+
+
+
+
+
+// TASK 5 USING COLSOLE  //
+// SIMULATE A NUMBER GUESSING GAME WHERE THE THE COMPUTER PICKS A RANDOM NUMBER FROM 1 TO 10 AND THE LOOP TRIES EACH NUMBER UNTIL IT FINDS THE CORRECT ONE // 
+// let myNum = 8;
+// for (let i = 0; i < 1; i++) {
+//     let randomNum = Math.floor(Math.random() * 10) + 1;
+//     if (myNum === randomNum) {
+//         console.log(`Congrats you predicted the correct number ${randomNum}`);
+//     } else {
+//         console.log(`Try again the correct number is ${randomNum}`);
+//     }
+// }
+
+
+
+
+
+
+    // TASK 5 USING ALERT  //
+//    let randomNum = Math.floor(Math.random() * 10) + 1;
+//   let response =  parseInt( prompt(`Enter any number that you predict`));
+//   if (response === randomNum) {
+//       alert(`Yah you predicted the correct number ${randomNum}`)
+      
+//   } else{
+//       alert(`Try again the correct number is ${randomNum}`)
+//   }
+    // TASK 5 USING ALERT  //
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+
+
+
+
+
+    // MY EXTRA WORK GENERATING BOTH EVEN AND ODD NUMBERS // 
+//  CONFUSION //
+// let count = 0;
+// for (let i = 0; i < 20; i++){
+//     count++;
+//     let random = Math.floor(Math.random() * 50 ) + 1;
+//     if (i % 2 === 0) {
+//         console.log(`Random Even Index: ${i}, Random Number: ${random} (Even: ${random % 2 === 0 ? 'Yes' : 'No'})`);
+        
+//     } else {
+//         console.log(`Random Odd Index: ${i}, Random Number: ${random} (Even: ${random % 2 === 0 ? 'Yes' : 'No'})`);
+        
+//     }
+// }
+//  console.log(`Loop ran ${count} times`);
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
